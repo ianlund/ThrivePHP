eee7cfc7bbceda20a26ef6ab1381f21c958725c8
Denver Timothy <denver@timothy.io>
2012-11-16 04:45:18 -0700

    correct file locations

--- views/_common/header.php
+++ views/_common/header.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/assets/css/screen.css">
-	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/assets/css/print.css" media="print">
-	<script src="<?= THRIVE_BASE ?>/assets/js/mootools-core.js"></script>
-	<script src="<?= THRIVE_BASE ?>/assets/js/mootools-more.js"></script>
-	<script src="<?= THRIVE_BASE ?>/assets/js/forms.js"></script>
-	<script src="<?= THRIVE_BASE ?>/assets/js/table.js"></script>
+	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/thrive/css/screen.css">
+	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/thrive/css/print.css" media="print">
+	<script src="<?= THRIVE_BASE ?>/thrive/js/mootools-core.js"></script>
+	<script src="<?= THRIVE_BASE ?>/thrive/js/mootools-more.js"></script>
+	<script src="<?= THRIVE_BASE ?>/thrive/js/forms.js"></script>
+	<script src="<?= THRIVE_BASE ?>/thrive/js/tables.js"></script>
 </head>
 <body>
