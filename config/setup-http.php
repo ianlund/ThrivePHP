b7a3ffe15a692c34b636e56e9618ac29c322742f
Denver Timothy <denver@timothy.io>
2012-11-16 04:13:04 -0700

    update defines to use thrive name

--- config/setup-http.php
+++ config/setup-http.php
@@ -5,21 +5,21 @@
 		setup defaults that are dependent on the web server
 
 		Using this URL: http://fqdn.com/app/index.php/path/to/view?key=value
-		URI_HOST:	fqdn.com
-		URI_BASE:	/app
-		URI_SELF:	/app/index.php/path/to/view
-		URI_SCRIPT:	/app/index.php
-		URI_INFO:	/path/to/view
-		URI_FULL:	/app/index.php/path/to/view?key=value
-		URI_QUERY:	key=value
+		THRIVE_HOST:	fqdn.com
+		THRIVE_BASE:	/app
+		THRIVE_SELF:	/app/index.php/path/to/view
+		THRIVE_SCRIPT:	/app/index.php
+		THRIVE_INFO:	/path/to/view
+		THRIVE_FULL:	/app/index.php/path/to/view?key=value
+		THRIVE_QUERY:	key=value
 	*/
-	define('URI_HOST', $_SERVER['HTTP_HOST']);
-	define('URI_BASE', (dirname($_SERVER['SCRIPT_NAME']) == '/' ? '' : dirname($_SERVER['SCRIPT_NAME'])));
-	define('URI_SELF', $_SERVER['PHP_SELF']);
-	define('URI_SCRIPT', $_SERVER['SCRIPT_NAME']);
-	define('URI_INFO', (isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : ''));
-	define('URI_FULL', $_SERVER['REQUEST_URI']);
-	define('URI_QUERY', $_SERVER['QUERY_STRING']);
+	define('THRIVE_HOST', $_SERVER['HTTP_HOST']);
+	define('THRIVE_BASE', (dirname($_SERVER['SCRIPT_NAME']) == '/' ? '' : dirname($_SERVER['SCRIPT_NAME'])));
+	define('THRIVE_SELF', $_SERVER['PHP_SELF']);
+	define('THRIVE_SCRIPT', $_SERVER['SCRIPT_NAME']);
+	define('THRIVE_INFO', (isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : ''));
+	define('THRIVE_FULL', $_SERVER['REQUEST_URI']);
+	define('THRIVE_QUERY', $_SERVER['QUERY_STRING']);
 	if(is_file(THRIVE_USER.'/config/config.php'))
 		require_once(THRIVE_USER.'/config/config.php');
 ?>
--- views/_common/header.php
+++ views/_common/header.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-	<link rel="stylesheet" href="<?= URI_BASE ?>/assets/css/screen.css">
-	<link rel="stylesheet" href="<?= URI_BASE ?>/assets/css/print.css" media="print">
-	<script src="<?= URI_BASE ?>/assets/js/mootools-core.js"></script>
-	<script src="<?= URI_BASE ?>/assets/js/mootools-more.js"></script>
-	<script src="<?= URI_BASE ?>/assets/js/forms.js"></script>
-	<script src="<?= URI_BASE ?>/assets/js/table.js"></script>
+	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/assets/css/screen.css">
+	<link rel="stylesheet" href="<?= THRIVE_BASE ?>/assets/css/print.css" media="print">
+	<script src="<?= THRIVE_BASE ?>/assets/js/mootools-core.js"></script>
+	<script src="<?= THRIVE_BASE ?>/assets/js/mootools-more.js"></script>
+	<script src="<?= THRIVE_BASE ?>/assets/js/forms.js"></script>
+	<script src="<?= THRIVE_BASE ?>/assets/js/table.js"></script>
 </head>
 <body>
