cfc599dc08af6c39d05ea6aaf1d1dff616c030c5
Denver Timothy <denver@timothy.io>
2012-11-16 04:45:04 -0700

    keep system stuff inside the system directory

--- index-default.php
+++ index-default.php
@@ -8,17 +8,4 @@
 	define('THRIVE_SYSTEM', THRIVE_ROOT.'/thrive');
 	define('THRIVE_USER', THRIVE_ROOT);
 	
-	try {
-		require_once(THRIVE_SYSTEM.'/config/setup-http.php');
-		$RTR = new Router();
-		require_once($RTR->viewPath);
-	} catch(Exception $e) {
-		$file = $line = null;
-		if(!headers_sent($file, $line)) {
-			print "<h1>404 Error</h1>".
-				"<pre>Message: ".$e->getMessage()."\n".print_r($e->getTraceAsString(), 1)."</pre>";
-		} else {
-			print "Headers sent. File: $file, line: $line";
-			print "<pre>".$e->getMessage()."\n".print_r($e->getTraceAsString(), 1)."</pre>";
-		}
-	}
+	require_once(THRIVE_SYSTEM.'/config/load.php');
